00d5d66c12fc85aef4f505de3217950cf414c87b
Bünyamin Ilhan <ilhan.buenyamin@gmail.com>
2023-05-12 22:56:00 +0300

    add function handleClick

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-/*let fields = [
-    null, 'circle', null,
-    'cross', null, null,
-    null, null, null
-];
-function init() {
-    render()
-};
-
-function render() {
-    let content = document.getElementById('content');
-    let tableHTML = '<table>'; // Beginn der Tabellen-HTML-Zeichenkette
-
-    for (let i = 0; i < 3; i++) {
-        tableHTML += '<tr>'; // Erzeugung einer neuen Tabellenzeile
-
-        for (let j = 0; j < 3; j++) {
-            tableHTML += '<td onclick="handleClick()">'; // Erzeugung einer neuen Tabellenspalte
-            let fieldValue = fields[i * 3 + j];
-
-            if (fieldValue === 'circle') {
-                tableHTML += generateCircleSVG(); // Hinzufügen eines Kreises ('O')
-            } else if (fieldValue === 'cross') {
-                tableHTML += generateXSVG(); // Hinzufügen eines Kreuzes ('X')
-            }
-
-            tableHTML += '</td>'; // Schließen der Tabellenzelle
-        }
-
-        tableHTML += '</tr>'; // Schließen der Tabellenzeile
-    }
-
-    tableHTML += '</table>'; // Schließen der Tabelle
-    content.innerHTML = tableHTML; // Einsetzen der Tabellen-HTML-Zeichenkette in den 'content'-Container
-}*/
 
 let fields = [
     null, null, null,
